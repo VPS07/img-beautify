c53ad9dda9e8e2d761d299d0addef77544d5565e
vps07 <vikysingh07860@gmail.com>
2024-01-15 14:20:42 +0530

    feat: enable PWA support with Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "assets/*"],
+      manifest: {
+        name: "IMGbeautify App",
+        description:
+          "IMGbeautify is a tool to add beautiful gradient background to your images",
+        short_name: "IMGbeautify App",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "./logo-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/logo-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        display: "standalone",
+      },
+    }),
+  ],
+});
